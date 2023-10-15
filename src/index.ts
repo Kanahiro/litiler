704b95915df615571d2b5dc098ad30ad3de758b1
Kanahiro <mediterranean1769@gmail.com>
2023-10-15 23:59:39 +0900

    workers

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { handle } from 'hono/aws-lambda';
-import { serve } from '@hono/node-server';
-
-import { getApp } from './server';
-const app = getApp(process.env);
-
-export const handler = handle(app);
-
-if (process.env.TITILER_USING_NODEJS === 'true') serve(app);
